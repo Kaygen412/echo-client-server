31a983a349918a51880e7e60e2893d88eeaead0c
Kaygen412 <kaygenroman@gmail.com>
2026-02-02 16:03:46 +0500

    Add files via upload

---
+++ echo-client.py
@@ -0,0 +1,8 @@
+import socket
+HOST = "10.90.14.46"
+PORT = 65432
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"hello,world")
+    data = s.recv(1024)
+print(f"Received {data!r}")
